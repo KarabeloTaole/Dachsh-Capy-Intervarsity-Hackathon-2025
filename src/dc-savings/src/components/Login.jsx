f65fc8a70bd2301a47ea282ca82899e627e4b22b
un4thi <u22553402@tuks.co.za>
2025-09-13 14:33:46 +0200

    handling the submission of the form data for the login page. for now we will use local storage

---
+++ src/dc-savings/src/components/Login.jsx
@@ -0,0 +1,9 @@
+const handleSubmit = () =>{
+    if(!FormData.username || !FormData.password){
+        alert("Please fill in all fields")
+    } else {
+        // proceed with login
+        setTimeout(() => {
+            
+    }
+}
