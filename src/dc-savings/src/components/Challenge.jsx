aadf436553570e74e355273d497833ac1c62b362
un4thi <u22553402@tuks.co.za>
2025-09-13 23:44:07 +0200

    added the component

--- src/dc-savings/src/components/Challenge.jsx
+++ src/dc-savings/src/components/Challenge.jsx
@@ -1,1 +1,7 @@
 //this is our challenge screen where users can keep track of their challenge progress
+import { useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, BarChart, Bar } from 'recharts';
+
+const ChallengeDetail = ({ challengeId, onBack }) => {
+    
+};
