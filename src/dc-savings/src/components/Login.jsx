039760deb66c2267624aab032a347faecfef253c
un4thi <u22553402@tuks.co.za>
2025-09-13 15:08:48 +0200

    finishing the login page

--- src/dc-savings/src/components/Login.jsx
+++ src/dc-savings/src/components/Login.jsx
@@ -8,6 +8,9 @@ const handleSubmit = () =>{
                 username: FormData.username, //storeing the user's username in local storag
                 LoginStatus: true, //indicating that the user is logged in
                 LoginTime: Date.now
-            })
-    }
+            }));
+     alert(`Welcome back, ${formData.username}! You have successfully logged in.`);
+      setIsLoading(false);        
+    }, 2000); // Simulate a network request
+}
 };
