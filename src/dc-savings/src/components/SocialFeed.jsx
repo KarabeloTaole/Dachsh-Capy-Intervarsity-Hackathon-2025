2c792183acb6d02a6a82a5cd2d462818ecbbf079
un4thi <u22553402@tuks.co.za>
2025-09-14 01:52:21 +0200

    creating initial component and useStates

--- src/dc-savings/src/components/SocialFeed.jsx
+++ src/dc-savings/src/components/SocialFeed.jsx
@@ -1,1 +1,10 @@
 //this is the social feed where users can interact with eachh other
+import { useState } from 'react';
+
+const SocialFeed = ({ onBack }) => {
+  const [newPost, setNewPost] = useState(''); // the useState for posts
+  const [filter, setFilter] = useState('all');
+  const [showCommentBox, setShowCommentBox] = useState(null);
+  const [newComment, setNewComment] = useState('');
+
+};
